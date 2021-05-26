2362f2c9537711845cae14a599bd023a1f05c690
ando3pan <79226850+ando3pan@users.noreply.github.com>
2021-05-25 23:54:58 -0700

    Update SubSubtree.java

--- SubSubtree.java
+++ SubSubtree.java
@@ -13,20 +13,29 @@ class Solution{
   public boolen isSubtree(TreeNode s, TreeNode t){
     if (s == null){
       return false;
+//if there's nothing, it obviously can't be a subtree
     }else if (isSametree(s,t)){
+//if sametree returns true with the nodes, then subtree will return true as well
       return true;
     }else{
       return isSubtree(s.left,t) || isSubtree(s.right, t);
+//keeping going through the trees to see if they match
     }
   }
   public boolean isSameTree(TreeNode s, TreeNode t){
     if (s==null || t==null){
-      return s == null && t ==null;
+//if either are null, they both have to be, so for the tree we have
+//they can not have any more branches
+      return s == null && t == null;
     }else if(s.val == t.val){
+//comparison operation to match the leading tree
       return isSametree(s.left,t.left) && isSameTree(s.right,t.right);
+//returns true only if both the left and right subtree are the isSametree
+//returns a comparison operation that would show True or false
     }else{
       return false;
     }
+//no matching? get rid of it
 
   }
 }
