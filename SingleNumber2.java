ac1537ba1085f45fe02ec7b8bf790e70a6e5c5e9
Anderson Pan <ando3pan@gmail.com>
2021-06-04 13:05:59 -0700

    Leetcode 137

---
+++ SingleNumber2.java
@@ -0,0 +1,21 @@
+class Solution{
+  public int singleNumber(int [] nums){
+    HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
+    for (int i : nums){
+      map.put(i,map.getOrDefault(i,0)+1)
+//specialty incase
+//put in i, then use map.get(i) to see if it's already in the list, if not
+//then it's default to 0, then we add 1 to the to it regardless of case
+
+//summary, puts in i and updates the occurence
+//or it gets i, then it sets its occruence of 1
+    }
+    for(int key: map.keySet()){
+      if(map.get(key) == 1){
+        return key;
+      }
+    }
+
+    return -1;
+  }
+}
