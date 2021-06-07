5d25b95cccfc9245810c0efddc383cb8ce3665ed
Anderson Pan <ando3pan@gmail.com>
2021-06-07 15:01:47 -0700

    Leetcode 844

---
+++ BackSpace_String_Compare.java
@@ -0,0 +1,29 @@
+public Solution{
+  public boolean backspaceCompare(String S, String T){
+    Stack<Character> sStack = new Stack<Character>();
+    for(char c: S.toCharArray()){
+      if(c != '#'){
+        sStack.push(c);
+      }else(if !sStack.isEmpty()){
+        sStack.pop();
+      }
+    }
+    Stack<Character> tStack = new Stack<Character>();
+    for(char c: T.toCharArray()){
+      if(c != '#'){
+        tStack.push(c);
+      }else(if !tStack.isEmpty()){
+        tStack.pop();
+      }
+    }
+//    return sStack == tStack
+    while(!sStack.isEmpty()){
+//empty out the stacks to compare until they are empty
+      char current = sStack.pop();
+      if(tStack.isEmpty() || tStack.pop() != current){
+        return false
+      }
+    }
+    return sStack.isEmpty() && sStack.isEmpty();
+  }
+}
