9bee6c2aa3c1b24cc98f9e0672fb5d1d3ad5e84d
Anderson Pan <ando3pan@gmail.com>
2021-06-17 10:38:40 -0700

    leetcode

--- pathSum.java
+++ pathSum.java
@@ -20,7 +20,7 @@ class Solution {
         }else if(root.left == null && root.right == null && sum - root.val == 0){
           return true;
         }else{
-          return hasPathSum(root.left,sum - root.val) || hasPathSum(root.right,sum - root.val)
+          return hasPathSum(root.left,sum - root.val) || hasPathSum(root.right,sum - root.val);
         }
     }
 }
