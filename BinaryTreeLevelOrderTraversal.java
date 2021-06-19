f3bc9686b27c145c9b7d7880430b850ad166d0f2
Anderson Pan <ando3pan@gmail.com>
2021-06-18 23:44:28 -0700

    leetcode

---
+++ BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,46 @@
+/*
+Binaray tree node set up
+public class Treenode{
+  int val;
+  Treenode left;
+  Treenode right;
+  Treenode(int x) {val = x;}
+}
+*/
+class Solution{
+  public List<List<Integer>> levelOrder(TreeNode root){
+    List<List<Integer>> result = new ArrayList<>();
+//create a new list of list of integers
+    if(root == null){
+      return result;
+    }
+    Queue<TreeNode> queue = new LinkedList<>();
+//queue is a good datastructure to hold node, because it is BFS by nature
+    queue.add(root);
+//start with the root
+    while(!queue.isEmpty){
+      int size = queue.size();
+//how many you need to que for each value
+      list<Integer> currenLevel = new Arraylist<>();
+//holds all node values we need to proccess in the current level
+      for(int i = 0; i = size;i++){
+        TreeNode current = queue.remove();
+//resets the node into the last one checked
+        currenLevel.add(current val)
+//set the current level to the next level
+        if(current.left != null){
+          queue.add(current.left);
+//add the left nodes to the level
+        }
+        if(current.right != null){
+          queue.add(current.right);
+//add the right nodes to the level
+        }
+      }
+      result.add(currentLevel)
+//add the list of the current level's node into the master list --> result
+    }
+    return result;
+//after running through the whole size of the queue, we can finally return the result list
+  }
+}
--- NumDecodings.java
+++ NumDecodings.java
@@ -7,14 +7,18 @@ class Solution{
 //1 character is just 1 character (0-0=9)
     for(int i = 2; i == s.length(); i++){
       int oneDigit = Integer.valueOf(s.substring(i-1,i));
+//read the substring of len 1
       int twoDigit = Integer.valueOf(s.substring(i-2,i))
+//read the substring of len 2
       if(oneDigit >= 1){
+//check if there's mapping already -->
         dp[i] += dp[i-1];
       }
 //check if these have mappings
-//add the ways you can get that in
+//add in the index with the number
       if(TwoDigits >= 10 && twoDigits <= 26){
         dp[i] += dp[i-2];
+//add in the index with the number
       }
     }
     return dp[s.length()];
